43a76b58989f2c0a35a05bf23ea309abc0a1e7c4
daichi.takagi <juams18@yahoo.co.jp>
2020-09-05 11:42:47 +0900

    add rakuten.class

--- src/controllers/Rakuten.java
+++ src/controllers/Rakuten.java
@@ -1,6 +1,11 @@
 package controllers;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -8,71 +13,152 @@ import javax.net.ssl.HttpsURLConnection;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-
 public class Rakuten {
 
+    //これなに？
     private final String USER__AGENT = "Mozilla/5.0";
 
+
+    //メインメソッド。変数入れて呼び出すメソッドに書き換える
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+
+    //public String getRakutenUrl(String keyword) {
+        //Raktenインスタンスの作成
         Rakuten api = new Rakuten();
+
+        //以下で定義しているRakutencredsをcredsで呼び出し。
+        //Rakutenのapiインスタンスにクレデンシャル情報を保存
         RakutenCreds creds = api.getCredentials();
-        System.out.println(creds.toString());
-        api.getFromKeyword(creds, "DEBUG HACKS");
+
+        //デバッグ用。クレデンシャル取得できてるか確認。
+       // System.out.println(creds.toString());
+
+        //左側にクレデンシャル情報入力。右側に検索文字列を入力。★ここを変数にする必要あり★
+        //修正前api.getFromKeyword(creds, "DEBUG HACKS");
+
+        //デバック用
+        String keyword = "サッカー戦術クロニクル";
+        String imageUrl = (String) api.getFromKeyword(creds, keyword);
+
+        //return imageUrl;
+
     }
 
-    private RakutenCreds getCredentials() {
+    //↓publicにしないと呼び出せないかも。★★
+    public RakutenCreds getCredentials() {
+
+        //RactenCredsのインスタンス作成
         RakutenCreds creds = new RakutenCreds();
+
+        //credsにload()メソッドを実行しクレデンシャル情報を入力（creds.appIDにID情報etc・・・）
         creds.load();
+
         return creds;
     }
 
-    public Map<String, Object> getFromKeyword(RakutenCreds creds, String keyword) {
-        Map<String, Object> res = new HashMap<String, Object>();
-        String urlRaw = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?";
+
+
+   //修正前public Map<String, Object> getFromKeyword(RakutenCreds creds, String keyword) {
+    public String getFromKeyword(RakutenCreds creds, String keyword) {
+        //楽天から受け取ったデータの入れ物。使用しなそうなのでコメントアウト
+       // Map<String, Object> res = new HashMap<String, Object>();
+
+        String urlRaw = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?";
+
+        //修正前String urlRaw = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?";
+
         try {
+
+            //StringBuilderで文字列を結合。検索用のURL作成
             StringBuilder sb = new StringBuilder();
             sb.append(urlRaw);
             sb.append("applicationId=");
             sb.append(URLEncoder.encode(creds.getAppID(),"UTF-8"));
             sb.append("&keyword=");
             sb.append(URLEncoder.encode(keyword,"UTF-8"));
-            System.out.println(sb.toString());
+
+            //デバック用
+        //   System.out.println(sb.toString());
+
+            //URL型のインスタンス作成。以下でurl型のメソッド実行するため。
             URL url = new URL(sb.toString());
 
+
+            //https接続を実行するインスタンスの作成？
             HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+
+            //URL要求のメソッドを次のどれかに設定(get,post,head,options...)
+            //ページを開くことをリクエストするのでGET
             con.setRequestMethod("GET");
+
+
+            //setDoOutput＝URL接続を出力用として使用する予定である場合はdoOutputフラグをtrueに設定し、そうでない場合はfalseに設定します。デフォルトはfalseです。
             con.setDoOutput(true);
 
+
+            //getOutputStream=この接続に書込みを行う出力ストリームを返します。  どういうこと？
+            //DataOutputStream=データ出力ストリームを使うと、アプリケーションはプリミティブ型のJavaデータを移植性のある形で出力ストリームに書き込むことができます。
+            //https接続するためにURLをstreamインスタンスに格納？このインスタンスができたくらいでstreamでURLへのアクセスがはじまる。
             DataOutputStream stream = new DataOutputStream(con.getOutputStream());
+
+            //streamを実行＝流れてきたデータをメモリに一括保存
             stream.flush();
+
             stream.close();
 
+            //getResponseCode=HTTP 応答メッセージから状態コードを取得します。(http接続が成功してるかどうか確認用？通信処理の場合分け用）
             int responseCode = con.getResponseCode();
+
+            //InputStreamReader=バイト・ストリームから文字ストリームへの橋渡しの役目を持ちます。バイトを読み込み、指定されたcharsetを使用して文字にデコードします
+            //getInputStream=この接続からの入力を受け取る入力ストリームを返します。
+            //conのURLで要求しかえってきたレスポンスをinに保持。
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
+
+            //文字列を扱うインスタンス。高速処理できる。
             StringBuilder response = new StringBuilder();
 
+            //readLine=1行読み込む。
+            //空白行に達したらWhile終了
             while ((inputLine = in.readLine()) != null) {
+
+                //読み込んだ文字列を[response]に追記
                 response.append(inputLine);
             }
-            System.out.println(response.toString());
+
+            //デバック用
+            //System.out.println(response.toString());
+
+            //RequestBuilderと同じようにMapに変換するのがよい★★
+            //ここに２を編集して追記///////////////////////////////////
+
+
+             ObjectMapper mapper = new ObjectMapper();
+             //以下String LargeImageurlまでコピペ。responseをmapにキャスト
+             Map reqMap =    mapper.readValue(response.toString(), new TypeReference<Map>() {});
+             //mapを配列にキャスト
+             ArrayList items = (ArrayList) reqMap.get("Items");
+             //配列をマップにキャスト
+             Map firstElem = (Map)items.get(0);
+             //検索して1つ目にヒットしたitemの情報をitemマップに保存
+             Map item = (Map)firstElem.get("Item");
+             //LargeImageUrlの値を保存。
+             String largeImageUrl = (String) item.get("largeImageUrl");
+
+             System.out.println("largeImageUrlは"+largeImageUrl);
+
             in.close();
 
+
         } catch (Exception e) {
-            // TODO 自動生成された catch ブロック
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
-        return res;
+        //デバック用.以下追記
+       // System.out.println("最後のreturnしているlargeImageUrlの中身は"+ largeImageUrl);
+        System.out.println("largeImageUrlは"+largeImageUrl);
+        return largeImageUrl;
     }
 
 }
--- src/controllers/RakutenCreds.java
+++ src/controllers/RakutenCreds.java
@@ -1,8 +1,10 @@
 package controllers;
+
 import java.io.InputStream;
 import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
+
 
 public class RakutenCreds {
 
@@ -10,6 +12,8 @@ public class RakutenCreds {
     private String secret;
     private String affID;
 
+
+    //tkg  getter setter
     public String getAppID() {
         return appID;
     }
@@ -40,8 +44,31 @@ public class RakutenCreds {
             InputStream inputStream = this.getClass()
               .getClassLoader()
               .getResourceAsStream("META-INF/config.yml");
+
+            //this.getclass：このrakutenCredsのクラス情報を取得する。（どんなプロパティを持ってるかなど。メタ情報）
+            //.getclassloader:このclassを読み込んだclassloaderを取得する
+            //                classloader：このプロジェクトで使用するクラスをまとめて取得する。(1プロジェクトで4つくらいある）
+            //                              classloaderで読み込んだものを他のclassでimportで参照して使えるようになる。
+            //.getResourceAsStream：classloaderが読めるフォルダ(自分の管理対象下)にある"META-INF～"を取得
+
             Map<String, Object> obj = yaml.load(inputStream);
-            System.out.println(obj.toString());
+            //キーがString型、valueがobject型(int,foatのようなプリミティブ型以外)　宣言
+            //マップ型のobjに.yamlファイル情報を入力
+            //yamlの左側がキー値。
+
+//        System.out.println(obj.toString());
+            //tkg デバッグ用と思われる
+
+  //         System.out.println(obj.get("application_id").toString());
+    //        System.out.println(obj.get("secret"));
+      //      System.out.println(obj.get("affiliateId"));
+
+
+
+
+            //.get（●●）で●●をキーとしてValueを取得する。
+            //ここのtoString()で文字列型に変換。toStringはこの下で定義しているtoString()メソッドではない？記載しなくてもいいやつ？
+            //https://www.kenschool.jp/blog/?p=4020
             this.setAppID(obj.get("application_id").toString());
             this.setSecret(obj.get("secret").toString());
             this.setAffID(obj.get("affiliateId").toString());
@@ -51,6 +78,8 @@ public class RakutenCreds {
         }
     }
 
+
+    //tkg stringbuilder.appendで文字列の連結 以下丸ごとデバック用？？それとも↑のthis.setAppIDの3行で参照されてる？
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{ ");
@@ -64,5 +93,8 @@ public class RakutenCreds {
         return sb.toString();
     }
 
+    //tkg　引数なしコンストラクタ
     public RakutenCreds(){}
+
+
 }
